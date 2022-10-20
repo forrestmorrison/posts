1a1b4a91c6d60e4f5df4f5e2adec1ee9577e6f26
forrestmorrison <forrestmorrison@gmail.com>
2022-10-20 13:09:43 -0500

    created PostsExcerpt

--- client/src/features/posts/PostsList.js
+++ client/src/features/posts/PostsList.js
@@ -1,10 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
 import { selectAllPosts, getPostsStatus, getPostsError, fetchPosts } from "./postsSlice";
 import { useEffect } from "react";
-
-import PostAuthor from "./PostAuthor";
-import TimeAgo from "./TimeAgo";
-import ReactionButtons from "./ReactionButtons";
+import PostsExcerpt from "./PostsExcerpt";
 
 const PostsList = () => {
     const dispatch = useDispatch();
@@ -22,15 +19,7 @@ const PostsList = () => {
     const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
     const renderedPosts = orderedPosts.map(post => (
-        <article key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.content.substring(0, 100)}</p>
-            <p className="postCredit">
-                <PostAuthor userId={post.userId} />
-                <TimeAgo timestamp={post.date} />
-            </p>
-            <ReactionButtons post={post} />
-        </article>
+        
     ))
 
     return (
